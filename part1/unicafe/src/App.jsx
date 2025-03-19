8b8cb62a836a3b2f3f52fc5c62ec2a3055f8e3dc
Zabalino <jcardenasz@unal.edu.co>
2025-03-19 02:27:36 -0500

    1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,6 +14,20 @@ const Total = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1> statistics </h1>
+      <p>good {props.good} </p>
+      <p>neutral {props.neutral} </p>
+      <p>bad {props.bad} </p>
+      <p>all {props.total} </p>
+      <p>average {props.average} </p>
+      <p>positive {props.positive} % </p>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [good, setGood] = useState(0)
   const [goodClicks, setGoodClicks] = useState(0)
@@ -24,23 +38,49 @@ const App = (props) => {
   const [bad, setBad] = useState(0)
   const [badClicks, setBadClicks] = useState(0)
 
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
   const goodClickHandler = () => {
     setGoodClicks( goodClicks+1 )
+    setAverage( (good * 1 + neutral * 0 + bad * (-1)) / ( goodClicks + neutralClicks + badClicks ) )
+    setPositive( (good/good + neutral + bad)*100 )
   }
   
   const neutralClickHandler = () => {
     setNeutralClicks( neutralClicks+1 )
+    setAverage( (good * 1 + neutral * 0 + bad * (-1)) / ( goodClicks + neutralClicks + badClicks ) )
+    setPositive( (good/good + neutral + bad)*100 )
   }
   
   const badClickHandler = () => {
     setBadClicks( badClicks+1 )
+    setAverage( (good * 1 + neutral * 0 + bad * (-1)) / ( goodClicks + neutralClicks + badClicks ) )
+    setPositive( (good/good + neutral + bad)*100 )
+  }
+
+  const averagePositiveHandlerGood = () => {
+    const updatedGood = good + 1    
+    const updatedGoodClicks = goodClicks + 1
+    setAverage( (updatedGood * 1 + neutral * 0 + bad * (-1)) / ( updatedGoodClicks + neutralClicks + badClicks ) )
+    setPositive( updatedGood/(updatedGood + neutral + bad)*100 )
+  }
+
+  const averagePositiveHandlerNeutral = () => {
+    const updatedNeutral = neutral + 1    
+    const updatedNeutralClicks = neutralClicks + 1
+    setAverage( (good * 1 + updatedNeutral * 0 + bad * (-1)) / ( goodClicks + updatedNeutralClicks + badClicks ) )
+    setPositive( good/(good + updatedNeutral + bad)*100 )
+  }
+
+  const averagePositiveHandlerBad = () => {
+    const updatedBad = bad + 1    
+    const updatedBadClicks = badClicks + 1
+    setAverage( (good * 1 + neutral * 0 + updatedBad * (-1)) / ( goodClicks + neutralClicks + updatedBadClicks ) )
+    setPositive( good/(good + neutral + updatedBad)*100 )
   }
 
   const title = 'give feedback'
-  const statistics = 'statistics'
-  const total = good + neutral + bad
-  const average = 'average'
-  const positive = 'positive'
 
   return (
     <div>
@@ -48,27 +88,31 @@ const App = (props) => {
 
       <Button onClick={() => {
         setGood( good+1 ),
-        goodClickHandler()
+        goodClickHandler(),
+        averagePositiveHandlerGood()
       }} text='good'/>
 
       <Button onClick={() => {
         setNeutral( neutral+1 ),
-        neutralClickHandler()
+        neutralClickHandler(),
+        averagePositiveHandlerNeutral()
       }} text='neutral'/>
 
       <Button onClick={() => {
         setBad( bad+1 ),
-        badClickHandler()
+        badClickHandler(),
+        averagePositiveHandlerBad()
       }} text='bad'/>
 
-      <h1> {statistics} </h1>
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        total={good + neutral + bad} 
+        average={average} 
+        positive={positive} 
+      />
 
-      <Total text='good' value={ good } />
-      <Total text='neutral' value={ neutral } />
-      <Total text='bad' value={ bad } />
-      <p>all { total } </p>
-      <p>{average} {total==0 ? 0 : (good * 1 + neutral * 0 + bad * (-1)) / ( total )} </p>
-      <p>{positive} {total==0 ? 0 : (good / total) * 100} % </p>
     </div>
   )
 }
